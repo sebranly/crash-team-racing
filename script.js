07e2c5653fb4ecad44c8c96770b98260ffbdaa1c
sebranly <sebranly.pinf@gmail.com>
2019-01-13 18:25:34 -0800

    chore: check if JSON object can be loaded online

---
+++ script.js
@@ -0,0 +1,24 @@
+var allItems = $.getJSON("data.json", function(data) {
+  var items = [];
+  $.each(data, function(key, val) {
+    items.push("<li id='" + key + "'>" + val + "</li>");
+  });
+  return items;
+});
+
+function updateList() {
+  $("#raw_list").html("<ul>" + allItems + "</ul>");
+}
+
+$(document).ready(function() {
+  $("#category_text_search").keypress(function (e) {
+    if (e.which == 13) {
+      $("#search_button").click();
+      return false;
+    }
+  });
+
+  $("#search_button").click(function() {
+    updateList();
+  });
+});
